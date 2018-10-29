6dbc5e4ffc391a620a1565e0de89ad5f235aa6da
Jack Duval <duval.j@husky.neu.edu>
2018-10-29 15:18:57 -0400

    Added ability to print machine id from excel sheet
    This works, takes input from row of excel sheet and then outputs machine id!

--- src/laundry_locator.py
+++ src/laundry_locator.py
@@ -1,3 +1,16 @@
+#Use pip install xlrd
+import xlrd
+
+#define variable linked to excel spreadsheet w data
+#to run on your pc you'll have to correct the path
+loc = ("/Users/jduval/Documents/GitHub/laundrylocator/src/exampleSheet.xlsx")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+sheet.cell_value(0,0)
+
+
 class machine():
     def __init__(self):
         self.buildingName = "";
@@ -10,9 +23,23 @@ class machine():
         print(self.buildingName + "-" + str(self.floor) + "-" + str(self.machineNumber) + "-" + self.controller + "-" + self.cscCode);
 
 exampleMachine = machine()
-exampleMachine.buildingName = "KER"
-exampleMachine.floor = 6
-exampleMachine.machineNumber = 1;
+exampleMachine.buildingName = "AAA"
+exampleMachine.floor = 5
+exampleMachine.machineNumber = 0;
 exampleMachine.cscCode = "xxx-xxx";
 
 exampleMachine.printInfo();
+
+#Example using sheet, should print data on first row of sheet
+#We can automate this process later
+exampleMachine2 = machine()
+row_array = sheet.row_values(1)
+
+
+exampleMachine2.buildingName = row_array[0]
+exampleMachine2.floor = row_array[1]
+exampleMachine2.machineNumber = row_array[2]
+exampleMachine2.controller = row_array[3]
+exampleMachine2.cscCode = row_array[4]
+
+exampleMachine2.printInfo();
